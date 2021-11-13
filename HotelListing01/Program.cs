b793b5e7787a29e28e5857d54717beb7c9f55319
kudlap <petr-kudla@seznam.cz>
2021-11-13 13:25:23 +0100

    Add project files.

---
+++ HotelListing01/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //serilog
+            Log.Logger = new LoggerConfiguration()
+            .WriteTo.File(
+                path: "C:\\Kurz\\WEB\\ASP.NET\\WebApp01\\logs\\log-.txt",
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}[{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day,
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information)
+            .CreateLogger();
+            try
+            {
+                Log.Information("Application Is Starting");
+                CreateHostBuilder(args).Build().Run();  //default settings
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application failed to start");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseSerilog() //changed
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
